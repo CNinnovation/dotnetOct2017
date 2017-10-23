5aab23d3038b6369ca9acda06fa4fa642d432abc
Christian Nagel <christian@christiannagel.com>
2017-10-23 13:21:52 +0200

    delegate sample

---
+++ DelegateSample/DelegateSample/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateSample
+{
+    class Calculator
+    {
+        public void Add(int x, int y) =>
+            Console.WriteLine($"{x} + {y} = {x + y}");
+
+        public void Subtract(int x, int y) =>
+            Console.WriteLine($"{x} - {y} = {x - y}");
+    }
+}
---
+++ DelegateSample/DelegateSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DelegateSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Action<int, int> mathop = calc.Add;
+            mathop += calc.Subtract;
+            mathop(7, 2);
+
+            mathop.Invoke(7, 2);
+            foreach (Action<int, int> mathop1 in mathop.GetInvocationList())
+            {
+                mathop1(11, 3);
+            }
+        }
+    }
+}
