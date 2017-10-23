5571b8a8b8e87c4a5d45426e011393736742b648
Christian Nagel <christian@christiannagel.com>
2017-10-23 10:35:07 +0200

    C# 6 sample

---
+++ CSharp6Samples/CSharp6Samples/MyStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp6Samples
+{
+    public static class MyStrings
+    {
+        public const string Foo = nameof(Foo);
+        public const string Bar = nameof(Bar);
+    }
+}
---
+++ CSharp6Samples/CSharp6Samples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+using static MyStrings;
+
+namespace CSharp6Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+
+            
+        }
+    }
+}
