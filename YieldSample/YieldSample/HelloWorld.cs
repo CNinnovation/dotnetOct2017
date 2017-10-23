cabc4f4c2e9084073560990b9269b086f835e73f
Christian Nagel <christian@christiannagel.com>
2017-10-23 13:22:00 +0200

    yield sample

---
+++ YieldSample/YieldSample/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YieldSample
+{
+    public class HelloWorld : IEnumerable<string>
+    {
+        public IEnumerator<string> GetEnumerator()
+        {
+            yield return "Hello";
+            yield return "World";
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ YieldSample/YieldSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace YieldSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 1, 2, 3, 4 };
+
+            foreach (int item in data)
+            {
+                Console.WriteLine(item);
+            }
+
+            IEnumerator enumerator = data.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                int item = (int)enumerator.Current;
+                Console.WriteLine(item);
+            }
+
+            var hw = new HelloWorld();
+
+            foreach (string s in hw)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
