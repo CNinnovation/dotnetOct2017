6acb698ad7606f3898b0e24b6a86f5f587447c74
Christian Nagel <christian@christiannagel.com>
2017-10-24 15:13:36 +0200

    logging sample

---
+++ LoggingSample/LoggingSample/Demo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggingSample
+{
+    public class Demo
+    {
+        private readonly ILogger<Demo> _logger;
+        public Demo(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Demo>();
+        }
+
+        public void Foo()
+        {
+            _logger.LogInformation("Foo called, info message");
+        }
+        public void Bar()
+        {
+            _logger.LogError("some error occurred, Bar");
+        }
+    }
+}
---
+++ LoggingSample/LoggingSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LoggingSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegisterServices();
+
+            Demo d1 = Container.GetService<Demo>();
+            d1.Foo();
+            d1.Bar();
+            
+        }
+
+        private static void RegisterServices()
+        {
+            var services = new ServiceCollection();
+            services.AddLogging(options =>
+            {
+                options.AddConsole();
+                options.AddDebug();
+            });
+            services.AddTransient<Demo>();
+            Container = services.BuildServiceProvider();
+        }
+
+        public static IServiceProvider Container { get; set; }
+    }
+}
