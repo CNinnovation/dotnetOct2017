84705fdcbe10a32f96dad7a83994524553bd44d2
Christian Nagel <christian@christiannagel.com>
2017-10-24 09:05:59 +0200

    infer names sample

---
+++ InferNames/InferNames/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InferNames
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+    }
+}
---
+++ InferNames/InferNames/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace InferNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b1 = new Book
+            {
+                Title = "Professional C#",
+                Publisher = "Wrox Press"
+            };
+
+            var b2 = new
+            {
+                Title = b1.Title,
+                Publisher = b1.Publisher
+            };
+
+            // infer members
+            var b3 = new
+            {
+                b1.Title,
+                b1.Publisher
+            };
+
+            Console.WriteLine(b3.Title);
+
+            var t1 =
+            (
+                Title: b1.Title,
+                Publisher: b1.Publisher
+            );
+            Console.WriteLine(t1.Title);
+
+            var t2 =
+            (
+                b1.Title,
+                b1.Publisher
+            );
+            Console.WriteLine(t2.Title);
+        }
+    }
+}
