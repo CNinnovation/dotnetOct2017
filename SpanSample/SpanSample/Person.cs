5dfe5d1308690bfa96774896b0aabdd04c7532c2
Christian Nagel <christian@christiannagel.com>
2017-10-24 09:06:06 +0200

    span

---
+++ SpanSample/SpanSample/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpanSample
+{
+    class Person
+    {
+        public Person(string name) => FirstName = name;
+
+        public string FirstName { get; set; }
+    }
+
+    class Book
+    {
+        public Book(string title, string publisher)
+        {
+            Title = title;
+            Publisher = publisher;
+        }
+        public string Title { get; }
+        public string Publisher { get; }
+    }
+
+    // record
+    //class Book(string Title, string Publisher)
+    //{
+    //}
+    // var b = new Book("Professional C# 6", "Wrox Press");
+    // var b2 = b with { Title = "Professional C# 7" }
+
+    // non-nullable reference types
+    // string? s = null; // compiler error
+}
---
+++ SpanSample/SpanSample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SpanSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x = 42;
+            Console.WriteLine(x.ToString());
+
+            Foo(x);
+
+            // ref types immer am heap
+            // val types am stack oder am heap (boxing)
+
+            // xx types only am stack
+            int[] data = { 1, 2, 3, 4 };
+            Span<int> span1 = new Span<int>(data);
+            Span<int> span2 = span1.Slice(2);
+            int x2 = span2[0];
+
+           // span1.GetHashCode();
+            // string s1 = span2.ToString();
+
+            Person[] people = new Person[]
+            {
+                new Person("Tom"), new Person("Turbo")
+            };
+            Span<Person> span3 = new Span<Person>(people);
+
+            Span<Person> span4 = span3.Slice(1);
+            Console.WriteLine($"{span4[0].FirstName}");
+            Console.WriteLine(x2);
+            
+        }
+        static void Foo(object o)  // boxing
+        {
+            Console.WriteLine(o.ToString());
+        }
+    }
+}
